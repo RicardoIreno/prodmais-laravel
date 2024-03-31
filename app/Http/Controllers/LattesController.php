19aea22f3ec4949e7c887c7b6f53beea5fd3379c
Tiago Murakami <trmurakami@gmail.com>
2024-03-30 22:17:49 -0300

    LattesController

--- app/Http/Controllers/LattesController.php
+++ app/Http/Controllers/LattesController.php
@@ -4,30 +4,45 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Person;
+use App\Models\Work;
 use Illuminate\Support\Facades\DB;
 
 class LattesController extends Controller
 {
 
-    public function createPerson(Object $curriculo, array $attributes)
+    public function artigos(array $artigos, array $attributes)
+    {
+        // echo "<pre>" . print_r($attributes, true) . "</pre>";
+        // echo "<pre>" . print_r($artigos, true) . "</pre>";
+        foreach ($artigos['ARTIGO-PUBLICADO'] as $artigo) {
+            echo "<pre>" . print_r($artigo, true) . "</pre>";
+            $createArtigo =  Work::updateOrCreate(
+                ['name' => $artigo['DADOS-BASICOS-DO-ARTIGO']['@attributes']['TITULO-DO-ARTIGO']]
+            );
+        }
+    }
+
+    public function createPerson(array $curriculo, array $attributes)
     {
         //echo "<pre>" . print_r($attributes, true) . "</pre>";
         //echo "<pre>" . print_r($curriculo, true) . "</pre>";
-        $lattesCV = get_object_vars($curriculo);
-        //dd($lattesCV['@attributes']['NOME-COMPLETO']);
-
-        // $person = DB::table('people')->insert([
-        //     'name' => $lattesCV['@attributes']['NOME-COMPLETO'],
-        //     'id' => $attributes['NUMERO-IDENTIFICADOR']
-        // ]);
-        // dd($person);
+        //dd($curriculo['@attributes']['NOME-COMPLETO']);
 
         $person = Person::updateOrCreate(
             ['id' =>  $attributes['NUMERO-IDENTIFICADOR']],
-            ['name' => $lattesCV['@attributes']['NOME-COMPLETO']]
+            ['name' => $curriculo['@attributes']['NOME-COMPLETO']]
         );
-        //dd($person);
-        dd($person);
+    }
+
+    public function producaoBibliografica(array $producaoBibliografica, array $attributes)
+    {
+        //echo "<pre>" . print_r($attributes, true) . "</pre>";
+        //echo "<pre>" . print_r($producaoBibliografica, true) . "</pre>";
+
+        if (isset($producaoBibliografica['ARTIGOS-PUBLICADOS'])) {
+            //echo "<pre>" . print_r($lattesPB['ARTIGOS-PUBLICADOS'], true) . "</pre>";
+            $this->artigos($producaoBibliografica['ARTIGOS-PUBLICADOS'], $attributes);
+        }
     }
 
     public function processXML(Request $request)
@@ -35,12 +50,14 @@ class LattesController extends Controller
         if ($request->file) {
             try {
                 $lattesXML = simplexml_load_file($request->file);
-                $lattes = get_object_vars($lattesXML);
-                //echo 'XML do Lattes recebido com sucesso!';
+                $lattes = json_decode(json_encode($lattesXML), true);
+                //$lattes = get_object_vars($lattesXML);
+                //echo "<pre>" . print_r($lattes['PRODUCAO-BIBLIOGRAFICA'], true) . "</pre>";
                 $this->createPerson($lattes['DADOS-GERAIS'], $lattes['@attributes']);
+                $this->producaoBibliografica($lattes['PRODUCAO-BIBLIOGRAFICA'], $lattes['@attributes']);
                 //echo "<pre>" . print_r($lattes['@attributes'], true) . "</pre>";
             } catch (\Exception $e) {
-                //echo 'O conteúdo recebido não é um XML do Lattes válido.';
+                echo 'O conteúdo recebido não é um XML do Lattes válido.';
             }
         } else {
             //echo 'Não foi enviado um arquivo XML do Lattes válido.';
