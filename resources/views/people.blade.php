17d6317af592ae96431ea66a56e186b5b654f11c
Tiago Murakami <trmurakami@gmail.com>
2024-03-30 23:03:50 -0300

    Layouts

--- resources/views/people.blade.php
+++ resources/views/people.blade.php
@@ -1,34 +1,20 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.layout')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Prodmais - Pesquisadores</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+@section('title', 'Prodmais - Pesquisadores')
 
-</head>
-
-<body>
-    <main class="container">
-        <h1>Prodmais - Pesquisadores</h1>
-        <div class="row">
-            <div class="col col-lg-4">
-                <h3>Refinar resultados <a href="people" class="btn btn-warning">Limpar busca</a> </h3>
-            </div>
-            <div class="col col-lg-8">
-                <h3>Pesquisadores</h3>
-                @foreach($people as $key => $value)
-                <p>{{ $value->name }}</p>
-                @endforeach
-            </div>
+@section('content')
+<main class="container">
+    <h1>Prodmais - Pesquisadores</h1>
+    <div class="row">
+        <div class="col col-lg-4">
+            <h3>Refinar resultados <a href="people" class="btn btn-warning">Limpar busca</a> </h3>
         </div>
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-
-</body>
-
-</html>
+        <div class="col col-lg-8">
+            <h3>Pesquisadores</h3>
+            @foreach($people as $key => $value)
+            <p>{{ $value->name }}</p>
+            @endforeach
+        </div>
+    </div>
+</main>
+@stop
--- resources/views/works.blade.php
+++ resources/views/works.blade.php
@@ -1,34 +1,20 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.layout')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Prodmais - Produção Intelectual</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+@section('title', 'Prodmais - Produção Intelectual')
 
-</head>
-
-<body>
-    <main class="container">
-        <h1>Prodmais - Produção Intelectual</h1>
-        <div class="row">
-            <div class="col col-lg-4">
-                <h3>Refinar resultados <a href="works" class="btn btn-warning">Limpar busca</a> </h3>
-            </div>
-            <div class="col col-lg-8">
-                <h3>Resultados</h3>
-                @foreach($works as $key => $value)
-                <p>{{ $value->name }}</p>
-                @endforeach
-            </div>
+@section('content')
+<main class="container">
+    <h1>Prodmais - Produção Intelectual</h1>
+    <div class="row">
+        <div class="col col-lg-4">
+            <h3>Refinar resultados <a href="works" class="btn btn-warning">Limpar busca</a> </h3>
         </div>
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-
-</body>
-
-</html>
+        <div class="col col-lg-8">
+            <h3>Resultados</h3>
+            @foreach($works as $key => $value)
+            <p>{{ $value->name }}</p>
+            @endforeach
+        </div>
+    </div>
+</main>
+@stop
