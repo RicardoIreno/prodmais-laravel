28aa0ebe94bcb4c46d411b10d946ee55dda3a8e2
Tiago Murakami <trmurakami@gmail.com>
2024-03-30 22:36:06 -0300

    Colunas

--- resources/views/people.blade.php
+++ resources/views/people.blade.php
@@ -13,9 +13,17 @@
 <body>
     <main class="container">
         <h1>Prodmais - Pesquisadores</h1>
-        @foreach($people as $key => $value)
-        <p>{{ $value->name }}</p>
-        @endforeach
+        <div class="row">
+            <div class="col col-lg-4">
+                <h3>Refinar resultados <a href="people" class="btn btn-warning">Limpar busca</a> </h3>
+            </div>
+            <div class="col col-lg-8">
+                <h3>Pesquisadores</h3>
+                @foreach($people as $key => $value)
+                <p>{{ $value->name }}</p>
+                @endforeach
+            </div>
+        </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
--- resources/views/works.blade.php
+++ resources/views/works.blade.php
@@ -13,9 +13,17 @@
 <body>
     <main class="container">
         <h1>Prodmais - Produção Intelectual</h1>
-        @foreach($works as $key => $value)
-        <p>{{ $value->name }}</p>
-        @endforeach
+        <div class="row">
+            <div class="col col-lg-4">
+                <h3>Refinar resultados <a href="works" class="btn btn-warning">Limpar busca</a> </h3>
+            </div>
+            <div class="col col-lg-8">
+                <h3>Resultados</h3>
+                @foreach($works as $key => $value)
+                <p>{{ $value->name }}</p>
+                @endforeach
+            </div>
+        </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
