c736c6422c2ad1dbef65b8f99f1fa8ab1f1303d2
Tiago Murakami <trmurakami@gmail.com>
2024-04-09 16:56:00 -0300

    CSS parcial

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,20 +4,17 @@
         <div class="sitemenu-container">
             <nav class="sitemenu" title="Menu do prodmais" aria-labelledby="Menu principal">
                 <ul class="sitemenu-list">
-                    <li class="sitemenu-item" title="Home"><a href="{{url('/')}}"
-                            class="nav-link px-2 text-secondary">Prodmais</a></li>
-                    <li class="sitemenu-item"><a class="sitemenu-link" href="{{url('/')}}"
-                            class="nav-link px-2 text-secondary">Início</a>
+                    <li class="sitemenu-item" title="Home"><a href="{{url('/')}}" class="nav-link px-2 text-secondary"><i class="i i-prodmaisheader siteheader__logo"></i></a>
                     </li>
-                    <li class=" sitemenu-item"><a class="sitemenu-link" href="{{url('/')}}/people"
-                            class="nav-link px-2 text-white">People</a>
+                    <li class="sitemenu-item"><a class="sitemenu-link" href="{{url('/')}}" class="nav-link px-2 text-secondary">Início <i class="i i-home sitemenu-ico"></i></a>
+                    </li>
+                    <li class=" sitemenu-item"><a class="sitemenu-link" href="{{url('/')}}/people" class="nav-link px-2 text-white">Pesquisadores <i class="i i-aboutme sitemenu-ico"></i></a>
+
                     </li>
 
-                    <li class=" sitemenu-item"><a class="sitemenu-link" href="{{url('/')}}/works"
-                            class="nav-link px-2 text-white">Works</a>
+                    <li class=" sitemenu-item"><a class="sitemenu-link" href="{{url('/')}}/works" class="nav-link px-2 text-white">Obras <i class="i i-about sitemenu-ico"></i></a>
                     </li>
-                    <li class=" sitemenu-item"><a class="sitemenu-link" href="{{url('/')}}/upload"
-                            class="nav-link px-2 text-white">Upload</a></li>
+                    <li class=" sitemenu-item"><a class="sitemenu-link" href="{{url('/')}}/upload" class="nav-link px-2 text-white">Upload <i class="i i-about sitemenu-ico"></i></a></li>
                 </ul>
             </nav>
         </div>
