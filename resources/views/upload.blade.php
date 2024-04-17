8463c5566c678be99a36ebc25b980f4314b9787a
Tiago Murakami <trmurakami@gmail.com>
2024-04-17 16:21:34 -0300

    Upload

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -5,15 +5,18 @@
 @section('content')
 <main class="c-wrapper-container">
     <div class="c-wrapper-paper">
-        <h2>Prodmais - Upload de XML do Lattes</h2>
-        <form action="{{url('lattes')}}" method="post" enctype="multipart/form-data">
-            <div class="mb-3">
-                @csrf
-                <label for="formLattesXML" class="form-label">Upload do arquivo curriculo.xml do Lattes</label>
-                <input class="form-control" type="file" id="formLattesXML" name='file'>
-            </div>
-            <button type="submit" class="btn btn-primary">Enviar XML</button>
-        </form>
+        <div class="c-wrapper-inner">
+            <h1 class="t t-h1">Prodmais - Upload</h1>
+            <h2 class="t t-h3 ">Inserir um XML do Lattes</h2>
+            <form action="{{url('lattes')}}" method="post" enctype="multipart/form-data">
+                <div class="mb-3">
+                    @csrf
+                    <label for="formLattesXML" class="form-label">Upload do arquivo curriculo.xml do Lattes</label>
+                    <input class="form-control" type="file" id="formLattesXML" name='file'>
+                </div>
+                <button type="submit" class="btn btn-primary">Enviar XML</button>
+            </form>
+        </div>
     </div>
 </main>
 @stop
