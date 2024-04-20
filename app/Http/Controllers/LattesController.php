9acf59e20928d3d4191ac0aca3d0f11dd76a6730
Tiago Murakami <trmurakami@gmail.com>
2024-04-20 12:14:57 -0300

    Correção

--- app/Http/Controllers/LattesController.php
+++ app/Http/Controllers/LattesController.php
@@ -627,6 +627,7 @@ class LattesController extends Controller
                 if (isset($lattes['PRODUCAO-BIBLIOGRAFICA']['ARTIGOS-ACEITOS-PARA-PUBLICACAO'])) {
                     $this->artigosAceitos($lattes['PRODUCAO-BIBLIOGRAFICA']['ARTIGOS-ACEITOS-PARA-PUBLICACAO'], $lattes['@attributes']);
                 }
+                return redirect('/person' . '/' . $lattes['@attributes']['NUMERO-IDENTIFICADOR']);
             } catch (\Exception $e) {
                 echo $e->getMessage();
             }
