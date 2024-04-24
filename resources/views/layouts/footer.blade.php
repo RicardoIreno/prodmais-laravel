d73985e48dd62a749437564c5f739df5b56ee973
Tiago Murakami <trmurakami@gmail.com>
2024-04-24 20:06:17 -0300

    Link

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,6 @@
 <footer class="sitefooter">
-    <p class="developed-by mt-3"><a href="https://github.com/unifesp/prodmais">Prodmais</a> é um Software livre
-        desenvolvido
-        originalmente pela Universidade Federal de São Paulo</p>
+    <p class="developed-by mt-3"><a href="https://github.com/trmurakami/prodmais-laravel">Prodmais</a> é um Software
+        livre desenvolvido originalmente pela Universidade Federal de São Paulo</p>
     <a href="https://unifesp.br/" target="_blank" class="sitefooter-link" title="Visite o site da Unifesp">
         <i class="i i-unifesp" alt="Logo da Unifesp"></i>
     </a>
